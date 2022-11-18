bb3da1fb2db8b32f2837c8967814a9dc40997c9f
AnthonyWork <schreiterjanthony@gmail.com>
2022-11-18 15:54:51 +0100

    removed stuff

--- index.js
+++
@@ -1,8 +0,0 @@
-function addEventlistenertoObjects() {
-  let figures = document.querySelectorAll("figure");
-  for (let i = 0; i < 32; i++) {
-    figures[i].addEventListener("click", function () {
-      console.log("Hello");
-    });
-  }
-}
